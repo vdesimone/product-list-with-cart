d035d3f8615f4bc0b213946adaf341316f5ec65c
vdesimone <173499012+vdesimone@users.noreply.github.com>
2025-02-21 14:04:26 -0500

    Set up `grid-template-columns` for `Menu` items and render `data.json` items in `Menu` component

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,6 +1,5 @@
 import data from "../data.json";
 import iconAddToCart from "../assets/images/icon-add-to-cart.svg";
-import cremeBruleeImage from "../assets/images/image-creme-brulee-desktop.jpg";
 
 function Menu() {
 
@@ -8,18 +7,20 @@ function Menu() {
     <div className="menu-container">
       <h1>Desserts</h1>
       <dl className="menu-items">
-        <dt className="item">
-          <img className="menu-img" src={cremeBruleeImage} alt="Creme Brulee Image" />
-          <button>
-            <img src={iconAddToCart} alt="Add to Cart Icon" />
-            Add to Cart
-          </button>
-          <div className="item-desc">
-            <dd className="category">Crème Brûlée</dd>
-            <dd className="name">Vanilla Bean Crème Brûlée</dd>
-            <dd className="price">$7.00</dd>
-          </div>
-        </dt>
+        {data.map((item) => (
+          <dt key={item} className="item">
+            <img className="menu-img" loading="lazy" src={item.image.desktop} alt={item.name} />
+            <button>
+              <img src={iconAddToCart} alt="Add to Cart Icon" />
+              Add to Cart
+            </button>
+            <div className="item-desc">
+              <dd className="category">{item.category}</dd>
+              <dd className="name">{item.name}</dd>
+              <dd className="price">${item.price.toFixed(2)}</dd>
+            </div>
+          </dt>
+        ))}
       </dl>
     </div>
   );
