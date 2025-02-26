e9ed94819466bab45bc033e38aa30fcef15765de
vdesimone <173499012+vdesimone@users.noreply.github.com>
2025-02-26 12:33:42 -0500

    Move `.item` JSX from `Menu` component to `MenuItem` for better modularity and state management

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,5 +1,5 @@
 import data from "../data.json";
-import iconAddToCart from "../assets/images/icon-add-to-cart.svg";
+import MenuItem from "./MenuItem";
 
 function Menu() {
 
@@ -8,18 +8,10 @@ function Menu() {
       <h1>Desserts</h1>
       <dl className="menu-items">
         {data.map((item) => (
-          <dt key={item} className="item">
-            <img className="menu-img" loading="lazy" src={item.image.desktop} alt={item.name} />
-            <button>
-              <img src={iconAddToCart} alt="Add to Cart Icon" />
-              Add to Cart
-            </button>
-            <div className="item-desc">
-              <dd className="category">{item.category}</dd>
-              <dd className="name">{item.name}</dd>
-              <dd className="price">${item.price.toFixed(2)}</dd>
-            </div>
-          </dt>
+          <MenuItem
+            key={item.index}
+            item={item}
+          />
         ))}
       </dl>
     </div>
