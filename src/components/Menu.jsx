39a3779623ac6a713f93e177d536f06c42a27249
vdesimone <173499012+vdesimone@users.noreply.github.com>
2025-03-03 10:35:20 -0500

    Update `Menu` component to use props and add `prop-types` validation

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,21 +1,27 @@
-import data from "../data.json";
 import MenuItem from "./MenuItem";
+import PropTypes from "prop-types";
 
-function Menu() {
+function Menu({ cartItems, updateItemQuantity }) {
 
   return (
     <div className="menu-container">
       <h1>Desserts</h1>
       <dl className="menu-items">
-        {data.map((item) => (
+        {cartItems.map((item) => (
           <MenuItem
             key={item.index}
             item={item}
+            updateItemQuantity={updateItemQuantity}
           />
         ))}
       </dl>
     </div>
   );
+}
+
+Menu.propTypes = {
+  cartItems: PropTypes.array.isRequired,
+  updateItemQuantity: PropTypes.func.isRequired,
 }
 
 export default Menu;
