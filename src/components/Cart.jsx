172ff14749dde09ec440e3f0548838a23119804f
vdesimone <173499012+vdesimone@users.noreply.github.com>
2025-03-03 10:56:13 -0500

    Add functionality and state management to `Cart` component

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,16 +1,70 @@
+import { useState, useEffect } from "react";
+import PropTypes from "prop-types";
 import emptyCartImage from "../assets/images/illustration-empty-cart.svg";
+import iconRemoveItem from "../assets/images/icon-remove-item.svg";
 
-function Cart() {
+function Cart({ cartItems, updateItemQuantity }) {
+
+  const allQuantityZero = cartItems.every((item) => item.quantity === 0);
+  const [cartEmpty, setCartEmpty] = useState(allQuantityZero);
+
+  useEffect(() => {
+    setCartEmpty(allQuantityZero);
+  }, [cartItems, allQuantityZero]);
+
+  const filteredCartItems = cartItems.filter(item => item.quantity > 0);
+  const sortedCartItems = [...filteredCartItems].sort((a, b) => {
+    return new Date(a.addedAt) - new Date(b.addedAt);
+  });
+
+  const totalItems = sortedCartItems.reduce((total, item) => total + item.quantity, 0);
+  const totalPriceOfItems = sortedCartItems.reduce((total, item) => {
+    return total + item.price * item.quantity;
+  }, 0).toFixed(2);
 
   return (
     <div className="cart-container">
-      <h2>Your Cart (0)</h2>
-      <div className="cart-contents">
-        <img className="empty-cart-img" src={emptyCartImage} alt="Empty Cart Illustration" />
-        <p className="empty-cart-text">Your added items will appear here</p>
-      </div>
+      <h2>Your Cart ({totalItems})</h2>
+      {cartEmpty ? (
+        <div className="cart-contents">
+          <img className="empty-cart-img" src={emptyCartImage} alt="Empty Cart Illustration" />
+          <p className="empty-cart-text">Your added items will appear here</p>
+        </div>
+      ) : (
+        <ul className="cart-contents">
+          {sortedCartItems.map((item) => {
+            const itemTotalPrice = (item.price * item.quantity).toFixed(2);
+            return (
+              <li key={item.index} className="item">
+                <h3 className="item-name">{item.name}</h3>
+                <div className="item-info">
+                  <p className="item-quantity">{item.quantity}x</p>
+                  <p className="item-price">@ ${item.price.toFixed(2)}</p>
+                  <p className="item-total-price">${itemTotalPrice}</p>
+                </div>
+                <div className="remove-item">
+                  <img
+                    src={iconRemoveItem}
+                    alt="Remove Item Icon"
+                    onClick={() => updateItemQuantity(item.index, 0)}
+                  />
+                </div>
+              </li>
+            );
+          })}
+          <div className="order-total">
+            <p>Order Total</p>
+            <h4>{totalPriceOfItems}</h4>
+          </div>
+        </ul>
+      )}
     </div>
   );
+}
+
+Cart.propTypes = {
+  cartItems: PropTypes.array.isRequired,
+  updateItemQuantity: PropTypes.func.isRequired,
 }
 
 export default Cart;
