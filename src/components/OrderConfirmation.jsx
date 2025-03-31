abe11303018bf1ae25d0fb907d110214bf10ac52
vdesimone <173499012+vdesimone@users.noreply.github.com>
2025-03-31 09:28:54 -0400

    Add and style ".order-info-container"

--- src/components/OrderConfirmation.jsx
+++ src/components/OrderConfirmation.jsx
@@ -19,32 +19,34 @@ function OrderConfirmation({ cartItems, updateOrderConfirmation }) {
         </div>
         <h2>Order Confirmed</h2>
         <p className="header-desc">We hope you enjoy your food!</p>
-        <ul className="order-info">
-          {sortedCartItems.map((item) => {
-            const itemTotalPrice = (item.price * item.quantity).toFixed(2);
-            return (
-              <li key={item.index} className="item">
-                <div className="info">
-                  <div className="details-container">
-                    <img src={item.image.thumbnail} alt={item.name}></img>
-                    <div className="details">
-                      <h3>{item.name}</h3>
-                      <div className="desc">
-                        <p className="quantity">{item.quantity}x</p>
-                        <p className="price">@ ${item.price.toFixed(2)}</p>
+        <div className="order-info-container">
+          <ul className="order-info">
+            {sortedCartItems.map((item) => {
+              const itemTotalPrice = (item.price * item.quantity).toFixed(2);
+              return (
+                <li key={item.index} className="item">
+                  <div className="info">
+                    <div className="details-container">
+                      <img src={item.image.thumbnail} alt={item.name}></img>
+                      <div className="details">
+                        <h3>{item.name}</h3>
+                        <div className="desc">
+                          <p className="quantity">{item.quantity}x</p>
+                          <p className="price">@ ${item.price.toFixed(2)}</p>
+                        </div>
                       </div>
                     </div>
+                    <p className="item-total-price">${itemTotalPrice}</p>
                   </div>
-                  <p className="item-total-price">${itemTotalPrice}</p>
-                </div>
-              </li>
-            );
-          })}
+                </li>
+              );
+            })}
+          </ul>
           <div className="order-total">
             <p>Order Total</p>
             <h4>${totalPriceOfItems}</h4>
           </div>
-        </ul>
+        </div>
         <div className="start-new-order">
           <button onClick={() => updateOrderConfirmation()}>Start New Order</button>
         </div>
