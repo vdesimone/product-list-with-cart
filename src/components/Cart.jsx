b5bf52ea124bd0ef48db1267b3acdb49e9e05680
vdesimone <173499012+vdesimone@users.noreply.github.com>
2025-04-30 10:09:49 -0400

    Add responsive styling for screens below 800px

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -24,53 +24,55 @@ function Cart({ cartItems, updateItemQuantity, updateOrderConfirmation }) {
   }, 0).toFixed(2);
 
   return (
-    <div className={cartEmpty ? "cart-container-empty" : "cart-container"}>
-      <h2>Your Cart ({totalItems})</h2>
-      {cartEmpty ? (
-        <div className="empty-cart-contents">
-          <img className="empty-cart-img" src={emptyCartImage} alt="Empty Cart Illustration" />
-          <p className="empty-cart-text">Your added items will appear here</p>
-        </div>
-      ) : (
-        <>
-          <ul className="cart-contents">
-            {sortedCartItems.map((item) => {
-              const itemTotalPrice = (item.price * item.quantity).toFixed(2);
-              return (
-                <li key={item.index} className="item">
-                  <div className="item-info">
-                    <h3>{item.name}</h3>
-                    <div className="item-details">
-                      <p className="item-quantity">{item.quantity}x</p>
-                      <p className="item-price">@ ${item.price.toFixed(2)}</p>
-                      <p className="item-total-price">${itemTotalPrice}</p>
+    <div className="cart">
+      <div className={cartEmpty ? "cart-container-empty" : "cart-container"}>
+        <h2>Your Cart ({totalItems})</h2>
+        {cartEmpty ? (
+          <div className="empty-cart-contents">
+            <img className="empty-cart-img" src={emptyCartImage} alt="Empty Cart Illustration" />
+            <p className="empty-cart-text">Your added items will appear here</p>
+          </div>
+        ) : (
+          <>
+            <ul className="cart-contents">
+              {sortedCartItems.map((item) => {
+                const itemTotalPrice = (item.price * item.quantity).toFixed(2);
+                return (
+                  <li key={item.index} className="item">
+                    <div className="item-info">
+                      <h3>{item.name}</h3>
+                      <div className="item-details">
+                        <p className="item-quantity">{item.quantity}x</p>
+                        <p className="item-price">@ ${item.price.toFixed(2)}</p>
+                        <p className="item-total-price">${itemTotalPrice}</p>
+                      </div>
                     </div>
-                  </div>
-                  <div className="remove-item">
-                    <img
-                      className="remove-item-icon"
-                      src={iconRemoveItem}
-                      alt="Remove Item Icon"
-                      onClick={() => updateItemQuantity(item.index, 0)}
-                    />
-                  </div>
-                </li>
-              );
-            })}
-          </ul>
-          <div className="order-total">
-            <p>Order Total</p>
-            <h4>${totalPriceOfItems}</h4>
-          </div>
-          <div className="delivery-info">
-            <img src={iconCarbonNeutral} alt="Carbon Neutral Icon" />
-            <p>This is a <span>carbon-neutral</span> delivery</p>
-          </div>
-          <div className="confirm-order">
-            <button onClick={() => updateOrderConfirmation()}>Confirm Order</button>
-          </div>
-        </>
-      )}
+                    <div className="remove-item">
+                      <img
+                        className="remove-item-icon"
+                        src={iconRemoveItem}
+                        alt="Remove Item Icon"
+                        onClick={() => updateItemQuantity(item.index, 0)}
+                      />
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+            <div className="order-total">
+              <p>Order Total</p>
+              <h4>${totalPriceOfItems}</h4>
+            </div>
+            <div className="delivery-info">
+              <img src={iconCarbonNeutral} alt="Carbon Neutral Icon" />
+              <p>This is a <span>carbon-neutral</span> delivery</p>
+            </div>
+            <div className="confirm-order">
+              <button onClick={() => updateOrderConfirmation()}>Confirm Order</button>
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 }
